f93a16f5e949c3400316e2a26dad53d200000f7f
indraghosh02 <indraghosh0802@gmail.com>
2024-04-27 10:55:36 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV8UsLas1PlyM-gLLI6-AiPyE2idBS3TQ",
+  authDomain: "painting-and-drawing-5bbe7.firebaseapp.com",
+  projectId: "painting-and-drawing-5bbe7",
+  storageBucket: "painting-and-drawing-5bbe7.appspot.com",
+  messagingSenderId: "652714286945",
+  appId: "1:652714286945:web:f3c3b0c7b2001a31a76f20"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
