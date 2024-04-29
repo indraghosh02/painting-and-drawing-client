188b36be2f144f518827c01ed07f6a5d105a951b
indraghosh02 <indraghosh0802@gmail.com>
2024-04-29 14:14:50 +0600

    env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -8,9 +8,6 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
  
 
-
-
-
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
