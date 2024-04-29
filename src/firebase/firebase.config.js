0824f3a10cdacc91a83dfadadf70669b3c1763d7
indraghosh02 <indraghosh0802@gmail.com>
2024-04-29 14:07:00 +0600

    last commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,22 +6,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAV8UsLas1PlyM-gLLI6-AiPyE2idBS3TQ",
-  authDomain: "painting-and-drawing-5bbe7.firebaseapp.com",
-  projectId: "painting-and-drawing-5bbe7",
-  storageBucket: "painting-and-drawing-5bbe7.appspot.com",
-  messagingSenderId: "652714286945",
-  appId: "1:652714286945:web:f3c3b0c7b2001a31a76f20"
+ 
 
 
 
 
-  // apiKey: import.meta.env.VITE_APIKEY,
-  // authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  // projectId: import.meta.env.VITE_PROJECTID,
-  // storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  // messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  // appId: import.meta.env.VITE_APPID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
